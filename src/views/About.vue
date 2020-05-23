3163038ac3e897dadb5459fd415d8da7c4492363
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-05-23 22:42:18 +0200

    create vue ruter project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+async function postData(url = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return response;
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HelloWorld msg=postData(https://swapi.dev/api/)/>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+
+async function postData(url = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return response;
+}
+
+
 
 export default {
   name: 'Home',
